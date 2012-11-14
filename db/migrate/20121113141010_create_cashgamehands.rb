467e5d20eb1a2927d2c51a84e4ab095738254de0
James Vanneman <jvannem@gmail.com>
2012-11-14 11:16:13 -0500

    added columns to holecards

--- db/migrate/20121113141010_create_cashgamehands.rb
+++
@@ -1,7 +0,0 @@
-class CreateCashgamehand < ActiveRecord::Migration
-  def change
-    create_table :cashgamehands do |t|
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20121113223845_create_gametypes.rb
+++
@@ -1,8 +0,0 @@
-class CreateGametypes < ActiveRecord::Migration
-  def change
-    create_table :gametypes do |t|
-
-      t.timestamps
-    end
-  end
-end
